23a7c9687e03dc1a1adb979dc06b14f691c9b9ea
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-06-14 23:53:09 +0600

    Cloudinary configuration and video uploading completed

---
+++ backend/src/video/video.schema.ts
@@ -0,0 +1,52 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document, Types } from "mongoose";
+
+@Schema({ timestamps: true })
+export class Video extends Document {
+  @Prop({ required: true })
+  title: string;
+
+  @Prop({ required: false, default: "" })
+  description: string;
+
+  @Prop({ required: true })
+  videoUrl: string;
+
+  @Prop({ required: false })
+  thumbnailUrl: string;
+
+  @Prop({ required: true })
+  publicId: string;
+
+  @Prop({ type: Types.ObjectId, ref: "User", required: true })
+  owner: Types.ObjectId;
+
+  @Prop({ default: 0 })
+  views: number;
+
+  @Prop({ default: [] })
+  likes: Types.ObjectId[];
+
+  @Prop({ default: [] })
+  dislikes: Types.ObjectId[];
+
+  @Prop({ default: [] })
+  tags: string[];
+
+  @Prop({ default: null })
+  duration: number; // in seconds
+
+  @Prop({ default: null })
+  format: string;
+
+  @Prop({ default: null })
+  resolution: string;
+
+  @Prop({ default: null })
+  frameRate: number;
+
+  @Prop({ default: null })
+  size: number; // in bytes
+}
+
+export const VideoSchema = SchemaFactory.createForClass(Video);
