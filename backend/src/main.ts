ec547a3f5b1548a19de20537da02c30a811676c9
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-06-15 09:01:00 +0600

    Server Dockerize completed

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,18 +1,18 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
-import * as morgan from 'morgan';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ConfigService } from "@nestjs/config";
+import * as morgan from "morgan";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = app.get(ConfigService).get<number>('PORT') || 4000;
+  const port = app.get(ConfigService).get<number>("PORT") || 4000;
 
   // middlewares
   app.enableCors();
-  app.setGlobalPrefix('/api/v1');
-  app.use(morgan('dev'));
+  app.setGlobalPrefix("/api/v1");
+  app.use(morgan("dev"));
 
   await app.listen(port);
-  console.log(`My Tube Server is running on ${await app.getUrl()}`);
+  console.log(`My Tube Server is running on http://localhost:${port}`);
 }
 bootstrap();
