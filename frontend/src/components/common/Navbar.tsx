046117e7a7d2e994f7348fabdcf69aeede2931a7
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-06-15 18:36:00 +0600

    Theme switcher implemented and dark-light theme applied

--- frontend/src/components/common/Navbar.tsx
+++ frontend/src/components/common/Navbar.tsx
@@ -1,12 +1,12 @@
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { Search, Bell, User } from "lucide-react";
+import { Search, Plus, User } from "lucide-react";
 import Image from "next/image";
 import { ModeToggle } from "./ModeToggle";
 
 const Navbar = () => {
   return (
-    <div className="w-full shadow-sm border-b">
+    <div className="w-full shadow-sm border-b bg-gray-100 dark:bg-gray-800 text-gray-800 dark:text-gray-200">
       <div className="container mx-auto px-4 py-3 flex items-center justify-between">
         <div className="flex items-center gap-2">
           <Image
@@ -27,8 +27,9 @@ const Navbar = () => {
         <div className="flex items-center gap-4">
           <ModeToggle />
 
-          <Button variant="ghost" size="icon">
-            <Bell className="h-5 w-5" />
+          <Button variant="ghost" className="ring-1">
+            <Plus className="h-5 w-5" />
+            <span>Create</span>
           </Button>
 
           <Button className="rounded-full ring-1" variant="ghost" size="icon">
