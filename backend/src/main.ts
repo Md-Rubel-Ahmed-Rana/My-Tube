7f7cb49f6467ae2602443667d9a59fc4fe6c43dc
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-06-15 23:17:48 +0600

    cors origings enable and logout API design done

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -9,7 +9,11 @@ async function bootstrap() {
   const port = app.get(ConfigService).get<number>("PORT") || 4000;
 
   // middlewares
-  app.enableCors();
+  app.enableCors({
+    origin: ["http://localhost:3000", "https://my-tube-pi-fawn.vercel.app"],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  });
   app.setGlobalPrefix("/api/v1");
   app.use(morgan("dev"));
   app.use(cookieParser());
