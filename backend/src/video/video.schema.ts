69a14439b37cabe329bec3600c16932100ed5d82
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-06-15 15:57:47 +0600

    Video CRUD operation completed

--- backend/src/video/video.schema.ts
+++ backend/src/video/video.schema.ts
@@ -1,7 +1,7 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { Document, Types } from "mongoose";
 
-@Schema({ timestamps: true })
+@Schema({ timestamps: true, versionKey: false, toJSON: { virtuals: true } })
 export class Video extends Document {
   @Prop({ required: true })
   title: string;
