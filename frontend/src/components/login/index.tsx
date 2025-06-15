cad4197a76e2846d161280520110e51fb089b731
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-06-15 19:56:01 +0600

    login and register page design completed

---
+++ frontend/src/components/login/index.tsx
@@ -0,0 +1,11 @@
+import LoginForm from "./LoginForm";
+
+const Login = () => {
+  return (
+    <div className="h-[90vh] max-w-md w-full mx-auto flex justify-center items-center ">
+      <LoginForm />
+    </div>
+  );
+};
+
+export default Login;
--- frontend/src/layout/RootLayout.tsx
+++ frontend/src/layout/RootLayout.tsx
@@ -17,6 +17,7 @@ const RootLayout = ({ children }: Props) => {
           maxWidth: "1440px",
           width: "100%",
         }}
+        className="bg-gray-100 dark:bg-gray-800 text-gray-800 dark:text-gray-200"
       >
         {children}
       </main>
