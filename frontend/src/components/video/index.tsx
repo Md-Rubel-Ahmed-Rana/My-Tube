c40548112edbb25cec0cfcadf54354bb9a524e1d
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-06-16 16:07:55 +0600

    Video player page design done

---
+++ frontend/src/components/video/index.tsx
@@ -0,0 +1,37 @@
+import { useGetSingleVideoQuery } from "@/features/videos";
+import { IVideo } from "@/types/video.type";
+import { useRouter } from "next/router";
+import VideoPlayer from "./VideoPlayer";
+import moment from "moment";
+import { formatDuration } from "@/utils/formatDuration";
+
+const Video = () => {
+  const { query } = useRouter();
+  const id = query?.id as string;
+  const { data, isLoading } = useGetSingleVideoQuery({ id });
+  const video = data?.data as IVideo;
+  return (
+    <>
+      {isLoading ? (
+        <div className="text-center">
+          <h3>Video loading...</h3>
+        </div>
+      ) : (
+        <div>
+          {video && <VideoPlayer video={video} />}
+          <div className="p-4 space-y-2">
+            <h2 className="text-xl font-semibold">{video.title}</h2>
+            <div className="flex flex-wrap items-center gap-4 text-sm text-muted-foreground">
+              <span>{video.views} views</span>
+              <span>{moment(video.createdAt).fromNow()}</span>
+              <span>{formatDuration(video.duration)}</span>
+            </div>
+            <p className="text-sm text-muted-foreground">{video.description}</p>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Video;
