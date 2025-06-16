a9d2186d874780b4253aa173ac92f0ee97e70fae
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-06-16 20:50:58 +0600

    Common video loading skeleton design for home page videos and dashboard my videos loading

--- frontend/src/components/dashboard/index.tsx
+++ frontend/src/components/dashboard/index.tsx
@@ -2,6 +2,7 @@ import { useGetVideosByOwnerQuery } from "@/features/videos";
 import DashboardHeader from "./DashboardHeader";
 import MyVideos from "./MyVideos";
 import { IVideo } from "@/types/video.type";
+import VideoLoadingSkeleton from "@/skeletons/Video-loading.skeleton";
 
 const Dashboard = () => {
   const { data, isLoading } = useGetVideosByOwnerQuery({});
@@ -9,13 +10,7 @@ const Dashboard = () => {
   return (
     <div className="p-2 lg:p-3 flex flex-col gap-2">
       <DashboardHeader totalVideos={videos?.length || 0} />
-      {isLoading ? (
-        <div className="text-center mt-5">
-          <h2>Videos loading...</h2>
-        </div>
-      ) : (
-        <MyVideos videos={videos} />
-      )}
+      {isLoading ? <VideoLoadingSkeleton /> : <MyVideos videos={videos} />}
     </div>
   );
 };
