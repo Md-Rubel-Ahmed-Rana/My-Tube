59375b2a7b5ddfbe93eea3ac33733f97e3a196e9
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-06-16 21:13:07 +0600

    video player page loading skeleton design done

--- frontend/src/skeletons/Video-loading.skeleton.tsx
+++ frontend/src/skeletons/Video-loading.skeleton.tsx
@@ -6,19 +6,19 @@ const VideoLoadingSkeleton = () => {
       {Array.from({ length: 16 }).map((_, index) => (
         <div
           key={index}
-          className="flex flex-col space-y-3 bg-gray-200 dark:bg-gray-700 rounded-md p-2"
+          className="flex flex-col space-y-3 rounded-md p-1 bg-gray-200 dark:bg-gray-800 border"
         >
-          <Skeleton className="h-[125px] w-full rounded-md" />
+          <Skeleton className="h-[125px] w-full rounded-md bg-gray-300 dark:bg-gray-700" />
           <div className="space-y-2">
-            <Skeleton className="h-4 w-[95%]" />
-            <Skeleton className="h-3 w-[80%]" />
+            <Skeleton className="h-4 w-[95%] bg-gray-300 dark:bg-gray-700" />
+            <Skeleton className="h-3 w-[80%] bg-gray-300 dark:bg-gray-700" />
           </div>
           <div className="flex justify-between items-center gap-2">
             <div className="flex items-center gap-3">
-              <Skeleton className="h-4 w-16" />
-              <Skeleton className="h-4 w-16" />
+              <Skeleton className="h-4 w-16 bg-gray-300 dark:bg-gray-700" />
+              <Skeleton className="h-4 w-16 bg-gray-300 dark:bg-gray-700" />
             </div>
-            <Skeleton className="h-4 w-24" />
+            <Skeleton className="h-4 w-24 bg-gray-300 dark:bg-gray-700" />
           </div>
         </div>
       ))}
