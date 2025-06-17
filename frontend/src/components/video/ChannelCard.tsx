ce9d02b64dcad2cd3757e2b7b903b4e7d484c8c8
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-06-17 18:40:13 +0600

    Showing channel/user info on video plaer page done

---
+++ frontend/src/components/video/ChannelCard.tsx
@@ -0,0 +1,34 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import { IUser } from "@/types/user.type";
+
+type Props = {
+  channel: IUser;
+};
+
+const ChannelCard = ({ channel }: Props) => {
+  return (
+    <div className="border rounded-lg p-2">
+      <div className="flex items-center gap-4 pb-2">
+        <Avatar className="w-14 h-14">
+          <AvatarImage src={channel?.photo} alt={channel?.name} />
+          <AvatarFallback>
+            {channel?.name
+              .split(" ")
+              .map((n) => n[0])
+              .join("")
+              .toUpperCase()
+              .slice(0, 2)}
+          </AvatarFallback>
+        </Avatar>
+        <div className="space-y-1">
+          <h2 className="text-lg">{channel?.name}</h2>
+          <p className="text-sm text-muted-foreground">@{channel?.username}</p>
+          <Button size="sm">View Channel</Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChannelCard;
