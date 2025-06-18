d7e4fc86e68a8d66f1a3cca81bee2a2b2c85700d
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-06-18 12:12:37 +0600

    Dislike API integration done

---
+++ frontend/src/components/video/LikeDislikeVideo.tsx
@@ -0,0 +1,68 @@
+import { ThumbsUp, Loader2, ThumbsDown } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import {
+  useDislikeAVideoMutation,
+  useLikeToAVideoMutation,
+} from "@/features/videos";
+import { handleApiMutation } from "@/utils/handleApiMutation";
+
+type Props = {
+  id: string;
+  totalLikes: number;
+  totalDisLikes: number;
+  actionType: "like" | "dislike";
+};
+
+const LikeDislikeVideo = ({
+  id,
+  totalLikes = 0,
+  totalDisLikes = 0,
+  actionType,
+}: Props) => {
+  const [like, { isLoading: liking }] = useLikeToAVideoMutation();
+  const [dislike, { isLoading: disliking }] = useDislikeAVideoMutation();
+
+  const isLoading: boolean = actionType === "like" ? liking : disliking;
+  const actionText: string = actionType === "like" ? "Like" : "Dislike";
+  const actionCount: number =
+    actionText === "Like" ? totalLikes : totalDisLikes;
+  const icon =
+    actionType === "like" ? (
+      <ThumbsUp className="w-4 h-4" />
+    ) : (
+      <ThumbsDown className="w-4 h-4" />
+    );
+
+  const handleLikeAVideo = async () => {
+    const mutation = actionType === "like" ? like : dislike;
+    await handleApiMutation(mutation, { id }, 200, {
+      error: "Failed to like",
+      success: "Liked video",
+    });
+  };
+
+  return (
+    <Tooltip>
+      <TooltipTrigger asChild>
+        <Button
+          onClick={handleLikeAVideo}
+          className="flex items-center gap-1"
+          disabled={isLoading}
+        >
+          {isLoading ? <Loader2 className="w-4 h-4 animate-spin" /> : icon}
+          <span className="text-sm">
+            {isLoading ? "" : `${actionText} (${actionCount})`}
+          </span>
+        </Button>
+      </TooltipTrigger>
+      <TooltipContent>{actionText} this video</TooltipContent>
+    </Tooltip>
+  );
+};
+
+export default LikeDislikeVideo;
