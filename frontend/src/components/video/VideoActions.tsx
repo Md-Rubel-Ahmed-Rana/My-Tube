1ac35a145d86faf629e746c2d739e66cd7a6d844
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-06-18 15:51:36 +0600

    Sharing video/video link to social platform feature added

--- frontend/src/components/video/VideoActions.tsx
+++ frontend/src/components/video/VideoActions.tsx
@@ -1,14 +1,8 @@
-import { Share2 } from "lucide-react";
-import { Button } from "@/components/ui/button";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
 import LikeDislikeVideo from "./LikeDislikeVideo";
 import { IVideo } from "@/types/video.type";
 import CopyLink from "./CopyLink";
 import DownloadVideo from "./DownloadVideo";
+import ShareVideo from "./ShareVideo";
 
 type Props = {
   video: IVideo;
@@ -39,15 +33,7 @@ const VideoActions = ({ video }: Props) => {
       </div>
 
       <div className="w-full lg:w-auto">
-        <Tooltip>
-          <TooltipTrigger asChild>
-            <Button size={"xs"} className="flex items-center gap-1 w-full">
-              <Share2 className="w-4 h-4" />
-              <span className="text-sm hidden lg:block">Share</span>
-            </Button>
-          </TooltipTrigger>
-          <TooltipContent>Share with others</TooltipContent>
-        </Tooltip>
+        <ShareVideo />
       </div>
 
       <div className="w-full lg:w-auto">
