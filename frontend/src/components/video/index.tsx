2dc6bd4c8a4a0ca5ecd1331ab319aba02f9c5db3
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-06-18 11:33:05 +0600

    Video tags color issue resolved

--- frontend/src/components/video/index.tsx
+++ frontend/src/components/video/index.tsx
@@ -34,7 +34,11 @@ const Video = () => {
             {video?.tags?.length > 0 && (
               <div className="mt-3 flex flex-wrap gap-1">
                 {video.tags.map((tag) => (
-                  <Badge key={tag} variant="outline">
+                  <Badge
+                    className="bg-gray-200 text-gray-800 dark:bg-gray-700 dark:text-gray-200"
+                    key={tag}
+                    variant="outline"
+                  >
                     #{tag}
                   </Badge>
                 ))}
