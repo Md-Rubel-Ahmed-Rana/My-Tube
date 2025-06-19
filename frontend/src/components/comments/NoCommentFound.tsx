a5821a910bf15ae7129bca43fcd131269ee2f4b7
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-06-19 23:59:15 +0600

    when a video does have comment and showing add comment issue fixed

--- frontend/src/components/comments/NoCommentFound.tsx
+++ frontend/src/components/comments/NoCommentFound.tsx
@@ -4,7 +4,7 @@ import AddCommentButton from "./AddCommentButton";
 
 const NoCommentFound = () => {
   return (
-    <Card className="w-full text-center py-8 bg-gray-300 dark:bg-gray-700">
+    <Card className="w-full text-center py-8 bg-gray-300 dark:bg-gray-700 mb-20">
       <CardContent className="flex flex-col items-center gap-4">
         <MessageSquareOff className="w-10 h-10 text-muted-foreground" />
         <h2 className="text-lg font-semibold">No comments yet</h2>
