7acedbab0a82b628338a721d97e8af10bcb74504
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-06-19 18:12:54 +0600

    Comment schema design done

---
+++ backend/src/comment/comment.schema.ts
@@ -0,0 +1,15 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { HydratedDocument, Types } from "mongoose";
+
+export type CommentDocument = HydratedDocument<Comment>;
+
+@Schema({ timestamps: true, versionKey: false, toJSON: { virtuals: true } })
+export class Comment {
+  @Prop()
+  text: string;
+
+  @Prop({ type: Types.ObjectId, ref: "User", required: true })
+  user: Types.ObjectId;
+}
+
+export const CommentSchema = SchemaFactory.createForClass(Comment);
