4908f9203c010bb44bcdef6d8a8a7b4fc48586a2
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-06-20 00:08:56 +0600

    Comment edit modal design done

--- frontend/src/components/comments/CommentCard.tsx
+++ frontend/src/components/comments/CommentCard.tsx
@@ -6,6 +6,8 @@ import { Pencil, Trash2 } from "lucide-react";
 import moment from "moment";
 import { useGetLoggedInUserQuery } from "@/features/auth";
 import { IUser } from "@/types/user.type";
+import { useState } from "react";
+import CommentEditModal from "./CommentEditModal";
 
 type Props = {
   comment: IComment;
@@ -15,41 +17,55 @@ const CommentCard = ({ comment }: Props) => {
   const { text, user, createdAt } = comment;
   const { data } = useGetLoggedInUserQuery({});
   const currentUser = data?.data as IUser;
+  const [isEdit, setIsEdit] = useState(false);
 
   return (
-    <Card className="w-full  border-b-0 border-r-0 border-l-0 border-t-gray-200 dark:border-t-gray-700 rounded-none p-2 shadow-gray-100 dark:shadow-gray-800  bg-gray-100 dark:bg-gray-800">
-      <div className="flex items-start justify-between">
-        <div className="flex items-center gap-3">
-          <Avatar className="w-10 lg:w-12 h-10 lg:h-12 rounded-full border">
-            <AvatarImage src={user?.photo || ""} />
-            <AvatarFallback className="bg-gray-200 dark:bg-gray-700">
-              {user.name?.slice(0, 2).toUpperCase()}
-            </AvatarFallback>
-          </Avatar>
-          <div className="flex flex-col">
-            <p className="font-medium">{user.name}</p>
-            <span className="text-sm text-muted-foreground">
-              {moment(createdAt).fromNow()}
-            </span>
+    <>
+      <Card className="w-full  border-b-0 border-r-0 border-l-0 border-t-gray-200 dark:border-t-gray-700 rounded-none p-2 shadow-gray-100 dark:shadow-gray-800  bg-gray-100 dark:bg-gray-800">
+        <div className="flex items-start justify-between">
+          <div className="flex items-center gap-3">
+            <Avatar className="w-10 lg:w-12 h-10 lg:h-12 rounded-full border">
+              <AvatarImage src={user?.photo || ""} />
+              <AvatarFallback className="bg-gray-200 dark:bg-gray-700">
+                {user.name?.slice(0, 2).toUpperCase()}
+              </AvatarFallback>
+            </Avatar>
+            <div className="flex flex-col">
+              <p className="font-medium">{user.name}</p>
+              <span className="text-sm text-muted-foreground">
+                {moment(createdAt).fromNow()}
+              </span>
+            </div>
           </div>
+
+          {currentUser?.id === user?.id && (
+            <div className="flex gap-2 items-center">
+              <Button
+                onClick={() => setIsEdit(true)}
+                variant="ghost"
+                size="icon"
+              >
+                <Pencil className="h-4 w-4" />
+              </Button>
+              <Button variant="ghost" size="icon">
+                <Trash2 className="h-4 w-4 text-red-500" />
+              </Button>
+            </div>
+          )}
         </div>
 
-        {currentUser?.id === user?.id && (
-          <div className="flex gap-2 items-center">
-            <Button variant="ghost" size="icon">
-              <Pencil className="h-4 w-4" />
-            </Button>
-            <Button variant="ghost" size="icon">
-              <Trash2 className="h-4 w-4 text-red-500" />
-            </Button>
-          </div>
-        )}
-      </div>
-
-      <CardContent className="pt-0 -mt-3">
-        <p className="text-sm leading-relaxed">{text}</p>
-      </CardContent>
-    </Card>
+        <CardContent className="pt-0 -mt-3">
+          <p className="text-sm leading-relaxed">{text}</p>
+        </CardContent>
+      </Card>
+      {isEdit && (
+        <CommentEditModal
+          open={isEdit}
+          setOpen={setIsEdit}
+          comment={{ id: comment?.id, text: comment?.text }}
+        />
+      )}
+    </>
   );
 };
 
---
+++ frontend/src/components/comments/CommentEditModal.tsx
@@ -0,0 +1,85 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+import { MessageCircle } from "lucide-react";
+import { useState } from "react";
+import { handleApiMutation } from "@/utils/handleApiMutation";
+import { useAddNewCommentMutation } from "@/features/comment";
+
+type Props = {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+  comment: { id: string; text: string };
+};
+
+const CommentEditModal = ({
+  open,
+  setOpen,
+  comment = { id: "", text: "" },
+}: Props) => {
+  const [text, setText] = useState(comment.text);
+  const [addComment, { isLoading }] = useAddNewCommentMutation();
+
+  const handleSubmit = async () => {
+    // await handleApiMutation(
+    //   addComment,
+    //   { comment: { text, video: id, user } },
+    //   201,
+    //   {
+    //     error: "Failed to add comment",
+    //     success: "Your comment posted successfully",
+    //   }
+    // );
+    setText("");
+    setOpen(false);
+  };
+
+  const handleClose = () => {
+    setText("");
+    setOpen(false);
+  };
+
+  return (
+    <Dialog open={open} onOpenChange={handleClose}>
+      <DialogContent className="sm:max-w-md bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100">
+        <DialogHeader>
+          <div className="flex items-center gap-2 mb-1">
+            <MessageCircle className="text-primary w-5 h-5" />
+            <DialogTitle>Edit Comment</DialogTitle>
+          </div>
+        </DialogHeader>
+
+        <div className="space-y-2">
+          <Textarea
+            placeholder="Type your comment here..."
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            rows={4}
+            className="resize-none"
+          />
+        </div>
+
+        <div className="flex justify-end gap-2 pt-4">
+          <Button onClick={handleClose} disabled={isLoading}>
+            Cancel
+          </Button>
+          <Button
+            onClick={handleSubmit}
+            disabled={
+              isLoading || !text.trim() || text.trim() === comment?.text.trim()
+            }
+          >
+            {isLoading ? "Saving..." : "Save changes"}
+          </Button>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default CommentEditModal;
