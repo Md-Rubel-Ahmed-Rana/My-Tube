acd3b0fa1a434ed36e610d1c3e4a7095634bbff3
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-06-20 16:49:02 +0600

    Playlist module completely finished

--- backend/src/playlist/playlist.module.ts
+++ backend/src/playlist/playlist.module.ts
@@ -1,9 +1,17 @@
-import { Module } from '@nestjs/common';
-import { PlaylistService } from './playlist.service';
-import { PlaylistController } from './playlist.controller';
+import { Module } from "@nestjs/common";
+import { PlaylistService } from "./playlist.service";
+import { PlaylistController } from "./playlist.controller";
+import { MongooseModule } from "@nestjs/mongoose";
+import { Playlist, PlaylistSchema } from "./playlist.schema";
+import { ConfigService } from "@nestjs/config";
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Playlist.name, schema: PlaylistSchema },
+    ]),
+  ],
   controllers: [PlaylistController],
-  providers: [PlaylistService],
+  providers: [PlaylistService, ConfigService],
 })
 export class PlaylistModule {}
