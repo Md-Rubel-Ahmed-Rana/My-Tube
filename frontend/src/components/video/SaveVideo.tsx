18b4987394aca4fd2933398ee1b74e3b81f4a5b8
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-06-21 23:33:05 +0600

    Video adding to playlist from video player page completed

---
+++ frontend/src/components/video/SaveVideo.tsx
@@ -0,0 +1,43 @@
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { Button } from "@/components/ui/button";
+import { ListVideo } from "lucide-react";
+import SaveToPlaylistModal from "../playlist-videos/SaveToPlaylistModal";
+import { useState } from "react";
+import { IVideo } from "@/types/video.type";
+
+type Props = {
+  video: IVideo;
+};
+
+const SaveVideo = ({ video }: Props) => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button
+            onClick={() => setIsOpen(true)}
+            className="flex items-center gap-1 w-full"
+            size={"xs"}
+          >
+            <ListVideo className="w-4 h-4" />
+            <span className="hidden lg:block">Save</span>
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent>Save to playlist</TooltipContent>
+      </Tooltip>
+
+      <SaveToPlaylistModal
+        open={isOpen}
+        setOpen={setIsOpen}
+        videoId={video.id}
+      />
+    </>
+  );
+};
+
+export default SaveVideo;
