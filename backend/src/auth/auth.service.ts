c0757ad31b9c0c80bfc4ff68ea4a651d06e46f05
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-06-23 09:43:40 +0600

    Google login/register API design done

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -5,6 +5,7 @@ import { UserService } from "src/user/user.service";
 import * as bcrypt from "bcrypt";
 import { ConfigService } from "@nestjs/config";
 import { Types } from "mongoose";
+import { GoogleLoginDto } from "./dto/google-login.dto";
 
 @Injectable()
 export class AuthService {
@@ -39,6 +40,40 @@ export class AuthService {
     );
 
     return `Bearer ${token}`;
+  }
+
+  async googleLogin(credentials: GoogleLoginDto): Promise<string> {
+    const isExist = await this.userService.getUserByEmailForGoogleLogin(
+      credentials.email
+    );
+
+    let accessToken: string;
+
+    if (isExist?._id) {
+      console.log("User exist", isExist);
+      const token = await this.jwtService.signAsync(
+        {
+          id: isExist?.id || isExist?._id,
+          email: isExist?.email,
+        },
+        { secret: this.configService.get<string>("JWT_SECRET") }
+      );
+
+      accessToken = `Bearer ${token}`;
+    } else {
+      const user = await this.userService.createUserWithGoogle(credentials);
+      console.log("User not exist. create user", user);
+      const token = await this.jwtService.signAsync(
+        {
+          id: user?.id || user?._id,
+          email: user?.email,
+        },
+        { secret: this.configService.get<string>("JWT_SECRET") }
+      );
+      accessToken = `Bearer ${token}`;
+    }
+
+    return accessToken;
   }
 
   async auth(id: Types.ObjectId) {
