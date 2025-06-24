980005019933ab6338758779e5837047832f3171
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-06-24 19:38:06 +0600

    video player page channel card responsive issue resolved

--- frontend/src/components/video/ChannelCard.tsx
+++ frontend/src/components/video/ChannelCard.tsx
@@ -9,28 +9,33 @@ type Props = {
 
 const ChannelCard = ({ channel }: Props) => {
   return (
-    <div className="border rounded-lg p-2">
-      <div className="flex items-center gap-4 pb-2">
-        <Link href={`/channel/${channel?.slug}?name=${channel?.name}`}>
-          <Avatar className="w-18 lg:w-10 h-8 lg:h-10">
-            <AvatarImage src={channel?.photo || ""} alt={channel?.name || ""} />
-            <AvatarFallback>
-              {channel?.name
-                .split(" ")
-                .map((n) => n[0])
-                .join("")
-                .toUpperCase()
-                .slice(0, 2)}
-            </AvatarFallback>
-          </Avatar>
-        </Link>
-        <div className="space-y-1">
+    <div className="border rounded-lg lg:p-2 p-1">
+      <div className="flex items-center lg:justify-start justify-between gap-4">
+        <div className="flex items-center gap-4">
           <Link href={`/channel/${channel?.slug}?name=${channel?.name}`}>
-            <h2 className="text-sm">{channel?.name || ""}</h2>
+            <Avatar className="w-8 lg:w-10 h-8 lg:h-10">
+              <AvatarImage
+                src={channel?.photo || ""}
+                alt={channel?.name || ""}
+              />
+              <AvatarFallback>
+                {channel?.name
+                  .split(" ")
+                  .map((n) => n[0])
+                  .join("")
+                  .toUpperCase()
+                  .slice(0, 2)}
+              </AvatarFallback>
+            </Avatar>
           </Link>
-          <small className="text-muted-foreground">
-            {channel?.subscriptions || 0} subscribers{" "}
-          </small>
+          <div className="space-y-1">
+            <Link href={`/channel/${channel?.slug}?name=${channel?.name}`}>
+              <h2 className="text-sm">{channel?.name || ""}</h2>
+            </Link>
+            <small className="text-muted-foreground">
+              {channel?.subscriptions || 0} subscribers{" "}
+            </small>
+          </div>
         </div>
         <SubscriptionButton channelId={channel?.id} />
       </div>
