72340fa8cae44254bb8cb4e37dbb4bf10651e7ae
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-06-24 12:46:04 +0600

    Channels for subscribe, unsubscribe APIs design done. And the feature fully completed

--- backend/src/channel/channel.module.ts
+++ backend/src/channel/channel.module.ts
@@ -3,12 +3,13 @@ import { ChannelService } from "./channel.service";
 import { ChannelController } from "./channel.controller";
 import { MongooseModule } from "@nestjs/mongoose";
 import { Channel, ChannelSchema } from "./channel.schema";
+import { ConfigService } from "@nestjs/config";
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Channel.name, schema: ChannelSchema }]),
   ],
   controllers: [ChannelController],
-  providers: [ChannelService],
+  providers: [ChannelService, ConfigService],
 })
 export class ChannelModule {}
