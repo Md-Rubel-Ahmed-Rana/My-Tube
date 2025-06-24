e1c6ceb40249bca228982ba66226408809ec8d3a
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-06-24 22:14:34 +0600

    getting total subscription for user done

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -4,12 +4,15 @@ import { UserController } from "./user.controller";
 import { MongooseModule } from "@nestjs/mongoose";
 import { User, UserSchema } from "./user.schema";
 import { ConfigService } from "@nestjs/config";
+import { ChannelService } from "src/channel/channel.service";
+import { ChannelModule } from "src/channel/channel.module";
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    ChannelModule,
   ],
   controllers: [UserController],
-  providers: [UserService, ConfigService],
+  providers: [UserService, ConfigService, ChannelService],
 })
 export class UserModule {}
