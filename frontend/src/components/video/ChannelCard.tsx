fff9294df51a23cec5d767b8cd4ca229e455856e
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-06-24 19:26:22 +0600

    subscribe and unsubscribe button design and APIs integratino done

--- frontend/src/components/video/ChannelCard.tsx
+++ frontend/src/components/video/ChannelCard.tsx
@@ -1,7 +1,7 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Button } from "@/components/ui/button";
 import { IUser } from "@/types/user.type";
 import Link from "next/link";
+import SubscriptionButton from "./SubscriptionButton";
 
 type Props = {
   channel: IUser;
@@ -11,23 +11,28 @@ const ChannelCard = ({ channel }: Props) => {
   return (
     <div className="border rounded-lg p-2">
       <div className="flex items-center gap-4 pb-2">
-        <Avatar className="w-10 lg:w-14 h-10 lg:h-14">
-          <AvatarImage src={channel?.photo || ""} alt={channel?.name || ""} />
-          <AvatarFallback>
-            {channel?.name
-              .split(" ")
-              .map((n) => n[0])
-              .join("")
-              .toUpperCase()
-              .slice(0, 2)}
-          </AvatarFallback>
-        </Avatar>
+        <Link href={`/channel/${channel?.slug}?name=${channel?.name}`}>
+          <Avatar className="w-18 lg:w-10 h-8 lg:h-10">
+            <AvatarImage src={channel?.photo || ""} alt={channel?.name || ""} />
+            <AvatarFallback>
+              {channel?.name
+                .split(" ")
+                .map((n) => n[0])
+                .join("")
+                .toUpperCase()
+                .slice(0, 2)}
+            </AvatarFallback>
+          </Avatar>
+        </Link>
         <div className="space-y-1">
-          <h2 className="text-lg">{channel?.name || ""}</h2>
           <Link href={`/channel/${channel?.slug}?name=${channel?.name}`}>
-            <Button size="sm">View Channel</Button>
+            <h2 className="text-sm">{channel?.name || ""}</h2>
           </Link>
+          <small className="text-muted-foreground">
+            {channel?.subscriptions || 0} subscribers{" "}
+          </small>
         </div>
+        <SubscriptionButton channelId={channel?.id} />
       </div>
     </div>
   );
---
+++ frontend/src/features/channel/index.ts
@@ -0,0 +1,34 @@
+import apiSlice from "../api";
+
+export const channelApi = apiSlice.injectEndpoints({
+  endpoints: (build) => ({
+    isSubscribedChannel: build.query({
+      query: ({ channelId }: { channelId: string }) => ({
+        url: `/channels/is-subscribed/${channelId}`,
+      }),
+      providesTags: ["channel", "video"],
+    }),
+    subscribeChannel: build.mutation({
+      query: ({ channelId }: { channelId: string }) => ({
+        url: `/channels/subscribe`,
+        method: "PATCH",
+        body: { channel: channelId },
+      }),
+      invalidatesTags: ["channel", "video"],
+    }),
+    unsubscribeChannel: build.mutation({
+      query: ({ channelId }: { channelId: string }) => ({
+        url: `/channels/unsubscribe`,
+        method: "PATCH",
+        body: { channel: channelId },
+      }),
+      invalidatesTags: ["channel", "video"],
+    }),
+  }),
+});
+
+export const {
+  useIsSubscribedChannelQuery,
+  useSubscribeChannelMutation,
+  useUnsubscribeChannelMutation,
+} = channelApi;
