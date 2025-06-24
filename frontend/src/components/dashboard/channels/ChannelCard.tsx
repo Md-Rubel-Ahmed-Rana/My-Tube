747b8a495550c07f4db6218883eb7419ab1e3549
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-06-25 00:07:59 +0600

    subscriptions count shown all the necessary places

--- frontend/src/components/dashboard/channels/ChannelCard.tsx
+++ frontend/src/components/dashboard/channels/ChannelCard.tsx
@@ -1,9 +1,11 @@
 import { IUser } from "@/types/user.type";
 import { Card, CardContent } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Users } from "lucide-react";
 import moment from "moment";
 import { UnsubscribeButton } from "./UnsubscribeButton";
+import Link from "next/link";
 
 type Props = {
   channel: IUser;
@@ -11,7 +13,7 @@ type Props = {
 
 const ChannelCard = ({ channel }: Props) => {
   return (
-    <Card key={channel.id} className="p-4 bg-gray-200 dark:bg-gray-800">
+    <Card key={channel?.id} className="p-4 bg-gray-200 dark:bg-gray-800">
       <CardContent className="flex items-center gap-4 p-0">
         <Avatar className="h-14 w-14">
           <AvatarImage src={channel?.photo} alt={channel?.name} />
@@ -29,7 +31,14 @@ const ChannelCard = ({ channel }: Props) => {
           <div className="text-xs text-muted-foreground">
             Joined {moment(new Date(channel.createdAt)).fromNow()}
           </div>
-          <UnsubscribeButton channelId={channel?.id || channel?._id} />
+          <div className="flex items-center gap-3 mt-3">
+            <Link href={`/channel/${channel?.slug}?name=${channel?.name}`}>
+              <Button size={"sm"} className="bg-gray-400 dark:bg-gray-700">
+                Visit channel
+              </Button>
+            </Link>
+            <UnsubscribeButton channelId={channel?.id || channel?._id} />
+          </div>
         </div>
       </CardContent>
     </Card>
