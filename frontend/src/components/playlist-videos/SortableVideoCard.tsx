c5b6885634688b49023d454d6f444856920e07f1
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-06-25 10:46:38 +0600

    navigate to wath video issue fixed and appended an icon to drag-and-drop

--- frontend/src/components/playlist-videos/SortableVideoCard.tsx
+++ frontend/src/components/playlist-videos/SortableVideoCard.tsx
@@ -3,6 +3,7 @@ import { CSS } from "@dnd-kit/utilities";
 import PlaylistVideoCardDesktop from "./PlaylistVideoCardDesktop";
 import PlaylistVideoCardMobile from "./PlaylistVideoCardMobile";
 import { IVideo } from "@/types/video.type";
+import { useRouter } from "next/router";
 
 type Props = {
   video: IVideo;
@@ -10,21 +11,38 @@ type Props = {
 };
 
 const SortableVideoCard = ({ video, playlistId }: Props) => {
-  const { attributes, listeners, setNodeRef, transform, transition } =
-    useSortable({ id: video.id });
+  const { attributes, setNodeRef, transform, transition } = useSortable({
+    id: video.id,
+  });
+  const { query, push } = useRouter();
+  const playlistslug = query?.playlistslug as string;
 
   const style = {
     transform: CSS.Transform.toString(transform),
     transition,
   };
 
+  const handleCardClick = () => {
+    console.log("click on video card");
+    push(
+      `/playlist/watch/${playlistslug}/video/${
+        video?.slug
+      }?title=${encodeURIComponent(video?.title)}`
+    );
+  };
+
   return (
-    <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
-      <div className="block lg:hidden">
+    <div
+      ref={setNodeRef}
+      style={style}
+      {...attributes}
+      className="cursor-pointer"
+    >
+      <div onClick={handleCardClick} className="block lg:hidden">
         <PlaylistVideoCardMobile video={video} playlistId={playlistId} />
       </div>
 
-      <div className="hidden lg:block">
+      <div onClick={handleCardClick} className="hidden lg:block">
         <PlaylistVideoCardDesktop video={video} playlistId={playlistId} />
       </div>
     </div>
