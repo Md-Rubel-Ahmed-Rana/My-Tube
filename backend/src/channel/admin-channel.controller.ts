fafdec138992d05f34218cc5a3aa930c743d9e20
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-07-03 16:16:54 +0600

    admin channel APIs design completed

--- backend/src/channel/admin-channel.controller.ts
+++ backend/src/channel/admin-channel.controller.ts
@@ -1,7 +1,37 @@
-import { Controller } from "@nestjs/common";
+import {
+  Controller,
+  Get,
+  Param,
+  Delete,
+  Patch,
+  Query,
+  Body,
+} from "@nestjs/common";
 import { ChannelService } from "./channel.service";
+import { Types } from "mongoose";
+import { QueryChannelDto } from "./dto/query-channel.dto";
 
 @Controller("admin/channels")
 export class AdminChannelController {
   constructor(private readonly channelService: ChannelService) {}
+
+  @Get()
+  async getAllChannels(@Query() query: QueryChannelDto) {
+    return this.channelService.getAllChannels(query);
+  }
+
+  @Get("stats")
+  async getChannelStats() {
+    return this.channelService.getChannelStats();
+  }
+
+  @Get(":id")
+  async getChannelById(@Param("id") id: string) {
+    return this.channelService.getChannelById(id);
+  }
+
+  @Delete(":id")
+  async deleteChannel(@Param("id") id: string) {
+    await this.channelService.deleteChannel(id);
+  }
 }
---
+++ backend/src/channel/dto/query-channel.dto.ts
@@ -0,0 +1,5 @@
+export class QueryChannelDto {
+  searchQuery?: string;
+  page?: number;
+  limit?: number;
+}
