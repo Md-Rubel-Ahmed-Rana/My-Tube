4fdc48de7c5c94b56cd879636989b10e69d8f4df
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-07-04 14:49:52 +0600

    navbar dropdown items rendering bug fixed

--- frontend/src/components/common/NavDropdown.tsx
+++ frontend/src/components/common/NavDropdown.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import Link from "next/link";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import {
@@ -6,106 +9,126 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import Link from "next/link";
-import { ModeToggle } from "./ModeToggle";
 import { useGetLoggedInUserQuery } from "@/features/auth";
-import { IUser } from "@/types/user.type";
-import LogoutButton from "./LogoutButton";
+import { useGetLoggedInAdminQuery } from "@/features/admin";
 import { formatNameForImageFallback } from "@/utils/formatNameForImageFallback";
-import { Menu } from "lucide-react";
+import { IUser } from "@/types/user.type";
+import { IAdmin } from "@/types/admin.type";
 import Spinner from "./Spinner";
+import LogoutButton from "./LogoutButton";
+import { ModeToggle } from "./ModeToggle";
+import { Menu } from "lucide-react";
 
 const NavDropdown = () => {
-  const { data, isLoading } = useGetLoggedInUserQuery({});
-  const user = data?.data as IUser;
+  const { data: userData, isLoading: isUserLoading } = useGetLoggedInUserQuery(
+    {}
+  );
+  const { data: adminData, isLoading: isAdminLoading } =
+    useGetLoggedInAdminQuery({});
 
+  const user = userData?.data as IUser;
+  const admin = adminData?.data as IAdmin;
+
+  const isLoading = isUserLoading || isAdminLoading;
+
+  // === Loading Spinner ===
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  // === Admin Shortcut Avatar ===
+  if (admin?.email) {
+    return (
+      <Link href="/admin/dashboard">
+        <Avatar className="h-8 lg:h-10 w-8 lg:w-10 cursor-pointer">
+          <AvatarImage src={admin.photo} alt="Admin image" />
+          <AvatarFallback>
+            {formatNameForImageFallback(admin.name)}
+          </AvatarFallback>
+        </Avatar>
+      </Link>
+    );
+  }
+
+  // === User Dropdown ===
+  if (user?.email) {
+    return (
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Avatar className="h-8 lg:h-10 w-8 lg:w-10 cursor-pointer">
+            <AvatarImage src={user.photo} alt="User image" />
+            <AvatarFallback>
+              {formatNameForImageFallback(user.name)}
+            </AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+
+        <DropdownMenuContent align="end" className="flex flex-col items-center">
+          <Link
+            href={`/profile/${user.slug || user.username}?name=${user.name}`}
+            className="w-full"
+          >
+            <DropdownMenuItem className="w-full cursor-pointer">
+              Profile
+            </DropdownMenuItem>
+          </Link>
+
+          <Link href="/dashboard/videos" className="w-full">
+            <DropdownMenuItem className="w-full cursor-pointer">
+              My Videos
+            </DropdownMenuItem>
+          </Link>
+
+          <Link href="/dashboard/playlists" className="w-full">
+            <DropdownMenuItem className="w-full cursor-pointer">
+              My Playlists
+            </DropdownMenuItem>
+          </Link>
+
+          <Link href="/video/upload" className="w-full">
+            <DropdownMenuItem className="w-full cursor-pointer">
+              Upload Video
+            </DropdownMenuItem>
+          </Link>
+
+          <LogoutButton />
+
+          <DropdownMenuItem className="w-full block lg:hidden">
+            <ModeToggle />
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    );
+  }
+
+  // === No user or admin ===
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <div>
-          {isLoading ? (
-            <Spinner />
-          ) : user?.id ? (
-            <Avatar className="h-8 lg:h-10 w-8 lg:w-10 cursor-pointer">
-              <AvatarImage src={user?.photo} alt="profile image" />
-              <AvatarFallback>
-                {formatNameForImageFallback(user?.name)}
-              </AvatarFallback>
-            </Avatar>
-          ) : (
-            <Button>
-              <Menu />
-            </Button>
-          )}
-        </div>
+        <Button variant="ghost" size="icon">
+          <Menu />
+        </Button>
       </DropdownMenuTrigger>
 
-      <DropdownMenuContent
-        align="end"
-        className="flex flex-col justify-center items-center"
-      >
-        {user?.id ? (
-          <>
-            <Link
-              href={`/profile/${user?.slug || user?.username}?name=${
-                user?.name
-              }`}
-              className="w-full mb-2 cursor-pointer"
-            >
-              <DropdownMenuItem className="w-full cursor-pointer">
-                Profile
-              </DropdownMenuItem>
-            </Link>
-            <Link
-              href={"/dashboard/videos"}
-              className="w-full mb-2 cursor-pointer"
-            >
-              <DropdownMenuItem className="w-full cursor-pointer">
-                My Videos
-              </DropdownMenuItem>
-            </Link>
-            <Link href={"/dashboard/playlists"} className="w-full mb-2">
-              <DropdownMenuItem className="cursor-pointer">
-                My Playlists
-              </DropdownMenuItem>
-            </Link>
-            <Link href={"/video/upload"} className="w-full mb-2 cursor-pointer">
-              <DropdownMenuItem className="w-full cursor-pointer">
-                Upload Video
-              </DropdownMenuItem>
-            </Link>
-            <LogoutButton />
-            <DropdownMenuItem className="w-full block lg:hidden">
-              <ModeToggle />
-            </DropdownMenuItem>
-          </>
-        ) : (
-          <>
-            <Link className="w-full" href={"/account/login"}>
-              <DropdownMenuItem className="cursor-pointer">
-                Login
-              </DropdownMenuItem>
-            </Link>
-            <Link className="w-full" href={"/admin/login"}>
-              <DropdownMenuItem className="cursor-pointer">
-                Login as admin
-              </DropdownMenuItem>
-            </Link>
-            <Link className="w-full" href={"/account/create"}>
-              <DropdownMenuItem className="cursor-pointer">
-                Register
-              </DropdownMenuItem>
-            </Link>
-            <Link href={"/video/upload"} className="w-full mb-2 cursor-pointer">
-              <DropdownMenuItem className="w-full cursor-pointer">
-                Upload Video
-              </DropdownMenuItem>
-            </Link>
-            <DropdownMenuItem className="block lg:hidden">
-              <ModeToggle />
-            </DropdownMenuItem>
-          </>
-        )}
+      <DropdownMenuContent align="end" className="flex flex-col items-center">
+        <Link className="w-full" href="/account/login">
+          <DropdownMenuItem className="w-full cursor-pointer">
+            Login
+          </DropdownMenuItem>
+        </Link>
+        <Link className="w-full" href="/admin/login">
+          <DropdownMenuItem className="w-full cursor-pointer">
+            Login as admin
+          </DropdownMenuItem>
+        </Link>
+        <Link className="w-full" href="/account/create">
+          <DropdownMenuItem className="w-full cursor-pointer">
+            Register
+          </DropdownMenuItem>
+        </Link>
+        <DropdownMenuItem className="block lg:hidden">
+          <ModeToggle />
+        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
