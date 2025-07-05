d73682bbf116465f90bcbed200377c12aacaae8a
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-07-05 11:20:15 +0600

    user activity module completed

---
+++ backend/src/user-activity/user-activity.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, UseGuards, Req } from "@nestjs/common";
+import { UserActivityService } from "./user-activity.service";
+import { AuthGuard } from "src/auth/auth.guard";
+
+@Controller("user-activity")
+export class UserActivityController {
+  constructor(private readonly userActivityService: UserActivityService) {}
+
+  @UseGuards(AuthGuard)
+  @Get()
+  findActivityForAUser(@Req() req: { user: { id: string } }) {
+    return this.userActivityService.findActivityForAUser(req?.user?.id);
+  }
+}
