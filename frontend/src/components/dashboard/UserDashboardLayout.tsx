c384107c09abe9b7e8563d17e224518efec7e21f
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-07-06 09:49:28 +0600

    Dashboard sidebar responsive issue fixed

--- frontend/src/components/dashboard/UserDashboardLayout.tsx
+++ frontend/src/components/dashboard/UserDashboardLayout.tsx
@@ -1,5 +1,5 @@
+import { useEffect, useState, ReactNode } from "react";
 import { SidebarProvider } from "@/components/ui/sidebar";
-import { ReactNode, useState } from "react";
 import UserSidebar from "./UserSidebar";
 import UserDashboard from ".";
 
@@ -9,6 +9,19 @@ type Props = {
 
 const UserDashboardLayout = ({ children }: Props) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const [isMobileDevice, setIsMobileDevice] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      const isMobile = window.innerWidth < 768;
+      setIsMobileDevice(isMobile);
+      setIsSidebarOpen(!isMobile);
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   return (
     <div className="bg-gray-100 dark:bg-gray-800 text-gray-800 dark:text-gray-200 min-h-screen w-full">
@@ -17,10 +30,17 @@ const UserDashboardLayout = ({ children }: Props) => {
           {/* Sidebar */}
           <div
             className={`h-screen sticky top-0 z-30 bg-white dark:bg-gray-900 border-r border-gray-200 dark:border-gray-700
-              transition-all duration-300 ease-in-out 
-              ${isSidebarOpen ? "w-64" : "w-0 overflow-hidden"}`}
+              transition-all duration-300 ease-in-out
+              ${
+                isSidebarOpen && !isMobileDevice
+                  ? "w-64"
+                  : "w-0 overflow-hidden"
+              }`}
           >
-            <UserSidebar setIsSidebarOpen={setIsSidebarOpen} />
+            <UserSidebar
+              setIsSidebarOpen={setIsSidebarOpen}
+              setIsMobileDevice={setIsMobileDevice}
+            />
           </div>
 
           {/* Main Content */}
