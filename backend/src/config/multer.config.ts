921ffd67b4ae2013f3558779ec396ad47d556369
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-07-10 09:24:42 +0600

    scoket.io implemented to send real-time video uploaded progress

---
+++ backend/src/config/multer.config.ts
@@ -0,0 +1,33 @@
+import { diskStorage } from "multer";
+import { join } from "path";
+import { existsSync, mkdirSync } from "fs";
+import { MulterOptions } from "@nestjs/platform-express/multer/interfaces/multer-options.interface";
+
+const uploadPath = join(__dirname, "../../uploads/videos");
+
+if (!existsSync(uploadPath)) {
+  mkdirSync(uploadPath, { recursive: true });
+}
+
+export const videoUploadConfig: MulterOptions = {
+  storage: diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, uploadPath);
+    },
+    filename: (req, file, cb) => {
+      const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+      const ext = file.originalname.split(".").pop();
+      cb(null, `${file.fieldname}-${uniqueSuffix}.${ext}`);
+    },
+  }),
+  limits: {
+    fileSize: 100 * 1024 * 1024, // 500MB
+  },
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype.startsWith("video/")) {
+      cb(null, true);
+    } else {
+      cb(new Error("Only video files are allowed!") as any, false);
+    }
+  },
+};
