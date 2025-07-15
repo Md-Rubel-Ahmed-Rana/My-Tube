954d62480bf6213df4f976e9f39d16f1aca2ac8d
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-07-16 00:19:44 +0600

    All about category done in admin dashboard

--- frontend/src/components/categories/CategoryActions.tsx
+++ frontend/src/components/categories/CategoryActions.tsx
@@ -9,6 +9,11 @@ import { ICategory } from "@/types/category.type";
 import { MoreVertical } from "lucide-react";
 import { useState } from "react";
 import CategoryUpdateModal from "./CategoryUpdateModal";
+import {
+  useDeleteCategoryMutation,
+  useUpdateCategoryMutation,
+} from "@/features/category";
+import { handleApiMutation } from "@/utils/handleApiMutation";
 
 type Props = {
   category: ICategory;
@@ -16,6 +21,21 @@ type Props = {
 
 const CategoryActions = ({ category }: Props) => {
   const [isEdit, setIsEdit] = useState(false);
+  const [remove, { isLoading: isDeleting }] = useDeleteCategoryMutation();
+  const [update, { isLoading: isUpdating }] = useUpdateCategoryMutation();
+
+  const handleDeleteCategory = async () => {
+    await handleApiMutation(remove, { id: category?.id }, 200);
+  };
+
+  const handleActiveDeactivateCategory = async () => {
+    await handleApiMutation(
+      update,
+      { id: category?.id, data: { is_active: !category.is_active } },
+      200
+    );
+  };
+
   return (
     <>
       <DropdownMenu>
@@ -25,10 +45,26 @@ const CategoryActions = ({ category }: Props) => {
           </Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent align="end">
-          <DropdownMenuItem onClick={() => setIsEdit(true)}>
+          <DropdownMenuItem
+            onClick={handleActiveDeactivateCategory}
+            disabled={isUpdating}
+            className="cursor-pointer"
+          >
+            {category?.is_active ? "Deactivate" : "Active"}
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            className="cursor-pointer"
+            onClick={() => setIsEdit(true)}
+          >
             Edit
           </DropdownMenuItem>
-          <DropdownMenuItem>Delete</DropdownMenuItem>
+          <DropdownMenuItem
+            className="cursor-pointer"
+            onClick={handleDeleteCategory}
+            disabled={isDeleting}
+          >
+            {isDeleting ? "Deleting..." : "Delete"}
+          </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
       {isEdit && (
