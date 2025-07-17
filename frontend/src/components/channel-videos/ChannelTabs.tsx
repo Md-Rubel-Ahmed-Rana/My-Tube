7b89c45e9cffb14856cbc0098a70eb9fc70eab9b
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-07-17 18:44:21 +0600

    Channel videos public route implemented

--- frontend/src/components/channel-videos/ChannelTabs.tsx
+++ frontend/src/components/channel-videos/ChannelTabs.tsx
@@ -1,20 +1,24 @@
 import { Button } from "@/components/ui/button";
+import { IUser } from "@/types/user.type";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useRouter } from "next/router";
 
-const ChannelTabs = () => {
+type Props = {
+  user: IUser;
+};
+
+const ChannelTabs = ({ user }: Props) => {
   const pathname = usePathname();
   const { asPath } = useRouter();
   const dynamicPath = asPath.split("?")[0];
-  const channelName = asPath.split("?")[1];
 
   const isActive = (path: string) =>
     pathname === path ? "!bg-blue-600 !text-white !hover:bg-blue-700" : "";
 
   return (
     <div className={`flex items-center w-full gap-2 overflow-x-auto`}>
-      <Link href={`/channel/videos?${channelName}`}>
+      <Link href={`/channel/videos/${user?.slug}?name=${user?.name}`}>
         <Button
           size={"sm"}
           className={isActive("/channel/videos") || isActive(dynamicPath)}
@@ -22,7 +26,7 @@ const ChannelTabs = () => {
           Videos
         </Button>
       </Link>
-      <Link href={`/channel/playlists?${channelName}`}>
+      <Link href={`/channel/playlists/${user?.slug}?name=${user?.name}`}>
         <Button size={"sm"} className={isActive("/channel/playlists")}>
           Playlists
         </Button>
---
+++ frontend/src/pages/channel/videos/[slug]/index.tsx
@@ -0,0 +1,23 @@
+import ChannelVideos from "@/components/channel-videos";
+import SEOHead from "@/components/common/SEOHead";
+import RootLayout from "@/layout/RootLayout";
+import { useRouter } from "next/router";
+import { ReactElement } from "react";
+
+const ChannelVideosPage = () => {
+  const { query } = useRouter();
+  const name = query?.name as string;
+
+  return (
+    <>
+      <SEOHead title={`Videos - ${name || "Unknown"}`} />
+      <ChannelVideos />
+    </>
+  );
+};
+
+export default ChannelVideosPage;
+
+ChannelVideosPage.getLayout = function (page: ReactElement) {
+  return <RootLayout>{page}</RootLayout>;
+};
