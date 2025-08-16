c8aa9adb746b77f6d39329feb2265731e79dc4da
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-08-16 18:28:08 +0600

    get all watch later videos Redux hook define and test done

--- frontend/src/components/watch-later-videos/index.tsx
+++ frontend/src/components/watch-later-videos/index.tsx
@@ -1,5 +1,12 @@
+import { useGetAllWatchLaterQuery } from "@/features/watch-later";
+import { IWatchLater } from "@/types/watch-later.type";
+
 const WatchLaterVideos = () => {
-  return <div>WatchLaterVideos</div>;
+  const { data, isLoading } = useGetAllWatchLaterQuery({});
+
+  const videos = (data?.data?.videos || []) as IWatchLater[];
+
+  return <div>WatchLaterVideos: {videos.length}</div>;
 };
 
 export default WatchLaterVideos;
